a3952654edb5fdaeabeeb3364ff3c940abf00e7d
Jeremy Hsu <jeremyhsu1@gmail.com>
2021-01-27 16:25:48 -0500

    Reformat DogsPage.js text

--- src/components/DogsPage.js
+++ src/components/DogsPage.js
@@ -14,11 +14,13 @@ export class DogsPage extends Component {
                 <Banner keywords={['My Two Puppies']} loop={false} />
                 <div className='block'>
                     <p className='multi-content'>
-                        Yup, I do have dogs. They're two fluffy boys named Bao Bao and Di Di. They're pretty great, and also
-                        sleep a ton. Good for them.
+                        Yup, I do have dogs. They're two fluffy boys named Bao Bao and Di Di. They're pretty great, and
+                        also sleep a ton. Good for them.
                     </p>
                     <p className='multi-content'>
-                        I also tend to procrastinate on putting stuff on this site. At least the link from the homepage routes to a new page now. Perhaps I'll post pictures of them before I graduate (I'll be a sophomore this fall).
+                        I also tend to procrastinate on putting stuff on this site. At least the link from the homepage
+                        routes to a new page now. Perhaps I'll post pictures of them before I graduate (I'll be a
+                        sophomore this fall).
                     </p>
                 </div>
             </div>
